1ab5f5feb8ff2d1fb77b682a6ba8d8f525275d8c
IwaalStar <kevinprasetia715@gmail.com>
2025-12-19 13:20:50 +0700

    detail pesan

--- resources/views/frontend/beranda.blade.php
+++ resources/views/frontend/beranda.blade.php
@@ -10,11 +10,11 @@
     @foreach ($items as $menu)
         <div class="card">
             <strong>{{ $menu->nama_menu }}</strong><br>
-            Harga: Rp {{ number_format($menu->harga) }}
-        <a href="{{ route('pemesanan.detail', 1) }}"
-   style="padding:6px 12px; background:#0d6efd; color:white; text-decoration:none;">
-   Lihat Detail Pesanan
-</a></div>
+            Harga: Rp {{ number_format($menu->harga) }}</div>
     @endforeach
+    <a href="{{ route('pemesanan.detail', 1) }}"
+   style="padding:6px 12px; background:#2c3e50; color:white; text-decoration:none;">
+   Lihat Detail Pesanan
+</a>
 @endforeach
 @endsection
